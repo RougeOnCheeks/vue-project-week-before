33abd8c0c6df28cd706cd2068ef5bfa2a86e86ea
RougeOnCheeks <lilianaaa804@gmail.com>
2022-08-02 11:46:19 +0900

    props 정리

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -4,11 +4,24 @@
   <p>isPublished: {{isPublished}}</p>
   <p>commentIds: {{commentIds}}</p>
   <p>author: {{author}}</p>
+
+<!--1. 부모컴포넌트에서 자식컴포넌트의 이벤트 직접 발생시키기-->
+  <button type="button" @click="childFunc" ref="btn">click</button>
+<!--3. 부모컴포넌트에서 자식컴포넌트의 데이터 옵션 값 직접 변경하기-->
+  <h1>{{msg}}</h1>
+<!--5. 부모컴포넌트에서 자식컴포넌트의 데이터 옵션 값 동기화하기-->
+  <button type="button" @click="childFunc2" ref="btn2">5. 자식 컴포넌트 데이터 변경</button>
 </template>
 
 <script>
 export default {
   name: "PageTitle",
+  data(){
+    return {
+      msg: '4. 자식 컴포넌트로 부터 보내는 메시지',
+      msg2 : '5. 초기 메시지',
+    };
+  },
   //props 유효성 검사
   props: {
     title: {
@@ -34,12 +47,29 @@ export default {
       }
     },
     contactsPromise: Promise,
-    customeValidator: {
+    customValidator: {
       validator: function(value){
         //값이 꼭 아래 세 문자열 중 하나와 일치해야 함
         return ['success', 'warning', 'danger'].indexOf(value) !== -1
       }
     }
+  },
+  methods: {
+    childFunc(){
+      console.log('1. 부모 컴포넌트에서 직접 발생시킨 이벤트');
+    },
+    callFromParent(){
+      console.log('2. 부모 컴포넌트에서 직접 호출한 함수');
+    },
+    childFunc2(){
+      this.msg2 = '5. 변경된 메시지';
+    }
+  },
+  /* 4. 자식컴포넌트에서 부모컴포넌트로 이벤트 / 데이터 전달하기
+  *   자식컴포넌트가 마운트되면 $emit을 통해 부모컴포넌트의 send-message 이벤트 호출, msg 데이터를 파라미터로 전송
+  * */
+  mounted() {
+    this.$emit('send-message', this.msg);
   }
 }
 </script>
--- src/views/NestedComponent.vue
+++ src/views/NestedComponent.vue
@@ -7,8 +7,14 @@
         :is-published="isShow()"
         :comment-ids="post.commentIds"
         :author="post.author"
-    />
+        @send-message="sendMessage" ref="child_component" /><!--ref는 유일한 키값을 사용해야 한다.-->
 
+  </div>
+  <div>
+    <button type="button" @click="changeChildData">3. Change Child Data</button>
+  </div>
+  <div>
+    <button type="button" @click="checkChild">5. 자식 컴포넌트 데이터 조회</button>
   </div>
 </template>
 
@@ -37,6 +43,29 @@ export default {
       } else{
         return false;
       }
+    },
+    /* 3. $ref로 자식컴포넌트에 접근, 데이터 옵션 직접 변경 */
+    changeChildData(){
+      this.$refs.child_component.msg = "3. 부모 컴포넌트가 변경한 데이터";
+    },
+    /* 4. 자식컴포넌트 마운트될때 send-message 이벤트 호출, msg 데이터 파라미터로 받음 */
+    sendMessage(data){
+      console.log(data);
+    },
+    checkChild(){
+      alert(this.msg2);
+    }
+  },
+  mounted() {
+    /* 1. 자식 컴포넌트 버튼 객체에 ref="btn"으로 접근할수 있도록 함 */
+    this.$refs.child_component.$refs.btn.click();
+    /* 2. $refs 사용하여 접근하게 되면 자식 컴포넌트 내에 정의된 모든 함수 호출 가능 */
+    this.$refs.child_component.callFromParent();
+  },
+  /* 5. computed 사용 시 자식 컴포넌트에 정의된 데이터 옵션 값의 변경사항을 항상 동기화 할 수 있음 */
+  computed:{
+    msg2(){
+      return this.$refs.child_component.msg2;
     }
   }
 }
